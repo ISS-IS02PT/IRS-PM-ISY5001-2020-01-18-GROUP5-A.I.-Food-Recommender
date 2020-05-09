33082e718e9d0057c2e00baf199ff30639174ac6
Mokky-ISS <e0529481@u.nus.edu>
2020-05-10 06:00:36 +0800

    Update bash scripts

--- SystemCode/Integrations/FoodApiClient/runLinux.sh
+++ SystemCode/Integrations/FoodApiClient/runLinux.sh
@@ -1,7 +1,4 @@
 cd FoodApiClient
 dotnet build
-#Linux
-xdg-open http://localhost:5000 &
-#Windows
-#start http://localhost:5000 &
-dotnet run ApiUri=http://0.0.0.0:8001/
+dotnet run ApiUri=http://localhost:8002/ &
+xdg-open http://localhost:5000
--- SystemCode/Integrations/FoodApiClient/runWin.sh
+++ SystemCode/Integrations/FoodApiClient/runWin.sh
@@ -1,7 +1,16 @@
 cd FoodApiClient
 dotnet build
+killbg() {
+        for p in "${pids[@]}" ; do
+                kill "$p";
+        done
+}
+trap killbg EXIT
+pids=()
 #Linux
 #xdg-open http://localhost:5000 &
 #Windows
-start http://localhost:5000 &
-dotnet run ApiUri=http://127.0.0.1:8000/
+start dotnet run ApiUri=http://127.0.0.1:8000/ &
+pids+=($!)
+start http://localhost:5000 
+
