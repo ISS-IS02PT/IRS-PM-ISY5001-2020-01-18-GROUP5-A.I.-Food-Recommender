c8c2a370eee15720fb9fc41e92fe6fbee1fa7d72
Mokky-ISS <e0529481@u.nus.edu>
2020-05-10 06:09:40 +0800

    Update runWin.sh

--- SystemCode/Integrations/FoodApiClient/runWin.sh
+++ SystemCode/Integrations/FoodApiClient/runWin.sh
@@ -1,16 +1,5 @@
 cd FoodApiClient
 dotnet build
-killbg() {
-        for p in "${pids[@]}" ; do
-                kill "$p";
-        done
-}
-trap killbg EXIT
-pids=()
-#Linux
-#xdg-open http://localhost:5000 &
-#Windows
 start dotnet run ApiUri=http://127.0.0.1:8000/ &
-pids+=($!)
 start http://localhost:5000 
 
