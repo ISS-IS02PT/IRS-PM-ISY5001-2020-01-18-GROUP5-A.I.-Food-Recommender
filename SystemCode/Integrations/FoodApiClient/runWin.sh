637d0f24e1f880a930daed03983bf8e29765aaee
Mokky-ISS <e0529481@u.nus.edu>
2020-05-10 16:37:13 +0800

    push debug folder

--- SystemCode/Integrations/FoodApiClient/runWin.sh
+++ SystemCode/Integrations/FoodApiClient/runWin.sh
@@ -1,9 +1,5 @@
 cd FoodApiClient
 dotnet build
-<<<<<<< Updated upstream
 start dotnet run ApiUri=http://localhost:8000/ &
-=======
-start dotnet run ApiUri=http://192.168.99.100:8000/ &
->>>>>>> Stashed changes
 start http://localhost:5000 
 
